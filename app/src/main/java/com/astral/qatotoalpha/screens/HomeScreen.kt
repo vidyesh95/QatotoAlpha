c49f2ea9478c5c0f50d1653f92897f1cf9336b2b
Vidyesh Churi <vidyesh95@gmail.com>
2024-02-05 23:53:52 +0530

    Top app bar v0.0.1

--- app/src/main/java/com/astral/qatotoalpha/screens/HomeScreen.kt
+++ app/src/main/java/com/astral/qatotoalpha/screens/HomeScreen.kt
@@ -1,15 +1,20 @@
 package com.astral.qatotoalpha.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -18,6 +23,7 @@ fun HomeScreen() {
     HomePage()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage() {
     BoxWithConstraints(
@@ -28,14 +34,37 @@ fun HomePage() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "Home Screen")
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.surface,
+                            titleContentColor = MaterialTheme.colorScheme.onSurface
+                        ),
+                        title = {
+                            Text(
+                                text = "Qatoto",
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                ScrollContent(innerPadding)
             }
         }
+    }
+}
+
+@Composable
+fun ScrollContent(innerPadding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .background(color = MaterialTheme.colorScheme.tertiaryContainer)
+    ) {
+
     }
 }
 
