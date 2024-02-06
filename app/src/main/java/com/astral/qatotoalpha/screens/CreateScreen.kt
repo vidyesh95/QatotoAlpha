53dd75b9d856d3164e4afd8507918798b55829a7
Vidyesh Churi <vidyesh95@gmail.com>
2024-02-06 22:20:08 +0530

    Create Top app bar v0.0.1

--- app/src/main/java/com/astral/qatotoalpha/screens/CreateScreen.kt
+++ app/src/main/java/com/astral/qatotoalpha/screens/CreateScreen.kt
@@ -1,39 +1,84 @@
 package com.astral.qatotoalpha.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.NotificationsNone
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.astral.qatotoalpha.ui.theme.RobotoSerifFontFamily
 
 @Composable
 fun CreateScreen() {
     CreatePage()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreatePage() {
     BoxWithConstraints(
         modifier = Modifier.fillMaxSize()
     ) {
-// A surface container using the 'background' color from the theme
+        // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "Create Screen")
+            val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+            Scaffold(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .nestedScroll(connection = scrollBehavior.nestedScrollConnection),
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "Qatoto",
+                                style = MaterialTheme.typography.titleLarge,
+                                fontFamily = RobotoSerifFontFamily,
+                                fontWeight = FontWeight.Normal
+                            )
+                        },
+                        actions = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.NotificationsNone,
+                                    contentDescription = "No Notifications"
+                                )
+                            }
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "Search"
+                                )
+                            }
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.AccountCircle,
+                                    contentDescription = "Account"
+                                )
+                            }
+                        },
+                        scrollBehavior = scrollBehavior
+                    )
+                }
+            ) { innerPadding ->
+                ScrollContent(innerPadding)
             }
         }
     }
