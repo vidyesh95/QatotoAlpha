e337b8b3d4755bb4ce1eb545715df8507d993606
Vidyesh Churi <vidyesh95@gmail.com>
2024-02-06 15:06:07 +0530

    Home Top app bar v0.0.2

--- app/src/main/java/com/astral/qatotoalpha/screens/HomeScreen.kt
+++ app/src/main/java/com/astral/qatotoalpha/screens/HomeScreen.kt
@@ -1,13 +1,18 @@
 package com.astral.qatotoalpha.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.NotificationsNone
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,7 +21,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
@@ -34,19 +41,40 @@ fun HomePage() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
+            val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
             Scaffold(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .nestedScroll(connection = scrollBehavior.nestedScrollConnection),
                 topBar = {
                     TopAppBar(
-                        colors = TopAppBarDefaults.topAppBarColors(
-                            containerColor = MaterialTheme.colorScheme.surface,
-                            titleContentColor = MaterialTheme.colorScheme.onSurface
-                        ),
                         title = {
                             Text(
                                 text = "Qatoto",
                                 style = MaterialTheme.typography.titleLarge
                             )
-                        }
+                        },
+                        actions = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.NotificationsNone,
+                                    contentDescription = "No Notifications"
+                                )
+                            }
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "Search"
+                                )
+                            }
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    imageVector = Icons.Default.AccountCircle,
+                                    contentDescription = "Account"
+                                )
+                            }
+                        },
+                        scrollBehavior = scrollBehavior
                     )
                 }
             ) { innerPadding ->
@@ -58,13 +86,18 @@ fun HomePage() {
 
 @Composable
 fun ScrollContent(innerPadding: PaddingValues) {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)
-            .background(color = MaterialTheme.colorScheme.tertiaryContainer)
     ) {
-
+        items(100) {
+            Text(
+                text = "Item $it",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
 
