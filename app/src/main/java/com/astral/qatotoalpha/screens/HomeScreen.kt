af36b6655926f22f91ac6a69cbe69f7b631feeab
Vidyesh Churi <vidyesh95@gmail.com>
2024-02-08 01:28:52 +0530

    Home Lazy column item

--- app/src/main/java/com/astral/qatotoalpha/screens/HomeScreen.kt
+++ app/src/main/java/com/astral/qatotoalpha/screens/HomeScreen.kt
@@ -1,15 +1,29 @@
 package com.astral.qatotoalpha.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.NotificationsNone
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,10 +35,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.astral.qatotoalpha.R
 import com.astral.qatotoalpha.ui.theme.RobotoSerifFontFamily
 
 @Composable
@@ -90,18 +109,162 @@ fun HomePage() {
 
 @Composable
 fun ScrollContent(innerPadding: PaddingValues) {
-    LazyColumn(
+    /*val resourceIds = listOf(
+        R.drawable.video_thumbnail_1,
+        R.drawable.video_thumbnail_2,
+        R.drawable.video_thumbnail_3,
+        R.drawable.video_thumbnail_4,
+    )*/
+    /*LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(innerPadding)
+            .padding(innerPadding),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(100) {
+        items(50) { item ->
             Text(
-                text = "Item $it",
+                text = "Item $item",
                 style = MaterialTheme.typography.bodyLarge,
                 modifier = Modifier.padding(16.dp)
             )
         }
+    }*/
+    VideoContainer(innerPadding = innerPadding)
+}
+
+@Composable
+fun VideoContainer(innerPadding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(innerPadding)
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(16f / 9f),
+            painter = painterResource(id = R.drawable.video_thumbnail_1),
+            contentDescription = "Video Thumbnail"
+        )
+        VideoDetails()
+        Divider(modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp))
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun VideoDetails() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(16.dp)
+                .width(36.dp)
+                .aspectRatio(ratio = 1f / 1f)
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    shape = CircleShape
+                )
+                .clip(shape = CircleShape),
+            painter = painterResource(id = R.drawable.profile_image_01),
+            contentDescription = "Video Thumbnail",
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .weight(1f)
+                .padding(top = 12.dp),
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                text = "Pomporo singing \uD83C\uDF3CFengzhi Senai\uD83C\uDF3C at Disney Land",
+                style = MaterialTheme.typography.labelLarge,
+                maxLines = 2,
+                softWrap = true,
+                overflow = TextOverflow.Ellipsis,
+            )
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = "Arin Light",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    maxLines = 1,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "•",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    maxLines = 1,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "2.5M",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    maxLines = 1,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "views",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    maxLines = 1,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "•",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    maxLines = 1,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "12 hours",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    maxLines = 1,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "ago",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    maxLines = 1,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+        Icon(
+            modifier = Modifier
+                .padding(end = 14.dp, top = 14.dp)
+                .width(14.dp)
+                .height(14.dp),
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = "More Options",
+            tint = MaterialTheme.colorScheme.onBackground
+        )
     }
 }
 
