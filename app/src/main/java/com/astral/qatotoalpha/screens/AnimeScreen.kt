b76ba8ba33746a7d85ce3c7ef9d5fe4251734177
Vidyesh Churi <vidyesh95@gmail.com>
2024-02-15 14:30:37 +0530

    anime screen v0.0.1

--- app/src/main/java/com/astral/qatotoalpha/screens/AnimeScreen.kt
+++ app/src/main/java/com/astral/qatotoalpha/screens/AnimeScreen.kt
@@ -1,11 +1,21 @@
 package com.astral.qatotoalpha.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.VolumeOff
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.NotificationsNone
 import androidx.compose.material.icons.outlined.Search
@@ -18,11 +28,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.astral.qatotoalpha.R
 import com.astral.qatotoalpha.ui.theme.RobotoSerifFontFamily
 
 @Composable
@@ -78,17 +95,118 @@ fun AnimePage() {
 
 @Composable
 fun AnimeScreenContent(innerPadding: PaddingValues) {
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(innerPadding)
     ) {
-        items(100) {
-            Text(
-                text = "Item $it",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(16.dp)
+        AnimeHero()
+        IconButtonRow()
+    }
+}
+
+@Composable
+fun AnimeHero() {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .fillMaxWidth()
+            .aspectRatio(ratio = 16f / 9f)
+            .clip(shape = MaterialTheme.shapes.medium)
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(ratio = 16f / 9f),
+            painter = painterResource(id = R.drawable.anime_hero_thumbnail),
+            contentDescription = "Anime Video Thumbnail",
+            contentScale = ContentScale.Crop
+        )
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.25f)))
+        Icon(
+            modifier = Modifier
+                .align(alignment = Alignment.TopEnd)
+                .padding(top = 8.dp, end = 6.dp)
+                .size(24.dp),
+            imageVector = Icons.AutoMirrored.Filled.VolumeOff,
+            contentDescription = "Volume Off",
+            tint = Color.White,
+        )
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(vertical = 8.dp, horizontal = 8.dp),
+            text = "A Record Of Mortal's Journey To Immortality: " +
+                    "Immortal Han Li's Adventure with cyan bottle",
+            color = Color.White,
+            style = MaterialTheme.typography.titleSmall,
+            softWrap = true,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Composable
+fun IconButtonRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(
+                vertical = 8.dp,
+                horizontal = 16.dp
             )
+    ) {
+        Column(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(40.dp),
+                painter = painterResource(id = R.drawable.genre),
+                contentDescription = "Genre"
+            )
+            Text(text = "Genre", style = MaterialTheme.typography.labelLarge)
+        }
+        Column(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(40.dp),
+                painter = painterResource(id = R.drawable.daily),
+                contentDescription = "Daily"
+            )
+            Text(text = "Daily", style = MaterialTheme.typography.labelLarge)
+        }
+        Column(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(40.dp),
+                painter = painterResource(id = R.drawable.favorite),
+                contentDescription = "Favorite"
+            )
+            Text(text = "Favorite", style = MaterialTheme.typography.labelLarge)
+        }
+        Column(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(40.dp),
+                painter = painterResource(id = R.drawable.ranking),
+                contentDescription = "Ranking"
+            )
+            Text(text = "Ranking", style = MaterialTheme.typography.labelLarge)
         }
     }
 }
@@ -109,5 +227,6 @@ fun AnimeScreenContent(innerPadding: PaddingValues) {
 )
 @Composable
 fun AnimeScreenPreview() {
-    AnimePage()
+    AnimeHero()
+    //IconButtonRow()
 }
